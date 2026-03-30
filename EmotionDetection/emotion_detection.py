a759766d40f590f5a9166e20c39d5c6ea2ea3f40
davidrijo <davidrijoo@gmail.com>
2026-03-29 23:58:16 -0400

    Task 7

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -8,8 +8,17 @@ def emotion_detector(text_to_analyze):
     
     response = requests.post(url, json=input_json, headers=headers)
     
-    formatted_response = json.loads(response.text)
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
     
+    formatted_response = json.loads(response.text)
     emotions = formatted_response['emotionPredictions'][0]['emotion']
     
     anger_score = emotions['anger']
@@ -17,10 +26,9 @@ def emotion_detector(text_to_analyze):
     fear_score = emotions['fear']
     joy_score = emotions['joy']
     sadness_score = emotions['sadness']
-    
     dominant_emotion = max(emotions, key=emotions.get)
     
-    result = {
+    return {
         'anger': anger_score,
         'disgust': disgust_score,
         'fear': fear_score,
@@ -28,5 +36,3 @@ def emotion_detector(text_to_analyze):
         'sadness': sadness_score,
         'dominant_emotion': dominant_emotion
     }
-    
-    return result
--- server.py
+++ server.py
@@ -8,17 +8,16 @@ def sent_detector():
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
 
-    anger = response['anger']
-    disgust = response['disgust']
-    fear = response['fear']
-    joy = response['joy']
-    sadness = response['sadness']
     dominant_emotion = response['dominant_emotion']
 
+    if dominant_emotion is None:
+        return "Invalid text! Please try again!"
+
     return (
-        f"For the given statement, the system response is 'anger': {anger}, "
-        f"'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. "
-        f"The dominant emotion is {dominant_emotion}."
+        f"For the given statement, the system response is "
+        f"'anger': {response['anger']}, 'disgust': {response['disgust']}, "
+        f"'fear': {response['fear']}, 'joy': {response['joy']} and "
+        f"'sadness': {response['sadness']}. The dominant emotion is {dominant_emotion}."
     )
 
 @app.route("/")
@@ -26,4 +25,4 @@ def render_index_page():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")
