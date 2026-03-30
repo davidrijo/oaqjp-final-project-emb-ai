a6c43fb28397116eecbf90b651e70ac35cee1ce2
davidrijo <davidrijoo@gmail.com>
2026-03-30 00:06:56 -0400

    Task 8

--- server.py
+++ server.py
@@ -1,3 +1,8 @@
+"""
+Executing this function initiates the application of emotion
+detection to be executed over the Flask channel and deployed on
+localhost:5000.
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +10,10 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_detector():
+    """
+    Analyzes the text string provided by the user and returns
+    the emotion scores and the dominant emotion.
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
 
@@ -22,7 +31,12 @@ def sent_detector():
 
 @app.route("/")
 def render_index_page():
+    """
+    This function initiates the rendering of the main application
+    page over the Flask channel.
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="localhost")
+    
